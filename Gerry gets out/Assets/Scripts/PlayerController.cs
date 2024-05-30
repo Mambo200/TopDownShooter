e92a4bcdd51c9674f0bdb46d376cb9a25d28c60a
Tobias Stroedicke <tobis-stroedicke@gmx.de>
2024-05-30 15:54:18 +0200

    Shoot logic
    And fix prefabs

--- Gerry gets out/Assets/Scripts/PlayerController.cs
+++ Gerry gets out/Assets/Scripts/PlayerController.cs
@@ -6,10 +6,16 @@ public class PlayerController : MonoBehaviour
 {
     #region private
     CharacterController p_CharacterController;
+    [SerializeField]
+    GameObject p_ShootStartPosition;
+    [SerializeField]
+    float p_ShootCooldown;
     #endregion
     #region public
     public int m_Speed;
     public int m_RotationSpeed;
+    [HideInInspector]
+    public float m_CurrentShootCooldown;
     #endregion
 
     // Start is called before the first frame update
@@ -21,15 +27,25 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Movement();
+        Rotation();
+
+        Shoot();
+    }
+
+    private void Movement()
+    {
         // get input
         float horizontalInput = Input.GetAxisRaw("Horizontal");
         float verticalInput = Input.GetAxisRaw("Vertical");
 
         // Player movement
-        Vector3 movement = new Vector3(horizontalInput,0, verticalInput);
+        Vector3 movement = new Vector3(horizontalInput, 0, verticalInput);
         movement = movement.normalized;
         p_CharacterController.Move(movement * Time.deltaTime * m_Speed);
-
+    }
+    private void Rotation()
+    {
         // Player rotation
         Vector3 mousePos = Input.mousePosition;
         mousePos.z = 5.23f;
@@ -39,6 +55,21 @@ public class PlayerController : MonoBehaviour
         mousePos.y = mousePos.y - objectPos.y;
 
         float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(new Vector3(0, -angle+90, 0));
+        transform.rotation = Quaternion.Euler(new Vector3(0, -angle + 90, 0));
+    }
+    private void Shoot()
+    {
+        // reduce shoot cooldown
+        m_CurrentShootCooldown = Mathf.Max(0, m_CurrentShootCooldown - Time.deltaTime);
+        if (m_CurrentShootCooldown > 0) return;
+
+        // could shoot. Check for key
+        float fire = Input.GetAxisRaw("Fire1");
+        // did not shoot
+        if (fire == 0) return;
+
+        // Shoot!
+        Debug.DrawRay(p_ShootStartPosition.transform.position, this.transform.forward * 10, Color.black, 1);
+        m_CurrentShootCooldown = p_ShootCooldown;
     }
 }
