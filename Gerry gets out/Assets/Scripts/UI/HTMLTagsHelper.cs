2b5d0c22b9945993ac77b86bb5bfe32cce8790bd
Tobias Stroedicke <tobis-stroedicke@gmx.de>
2024-06-09 21:55:41 +0200

    Added Helper for Textmesh pro text formats

---
+++ Gerry gets out/Assets/Scripts/UI/HTMLTagsHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.UI
+{
+    public static class HTMLTagsHelper
+    {
+        /// <summary>Bold / Fett</summary>
+        public static string StartBold() => "<b>";
+        /// <summary>Bold / Fett</summary>
+        public static string EndBold() => "</b>";
+
+        /// <summary>Italic / Kursiv</summary>
+        public static string StartItalic() => "<i>";
+        /// <summary>Italic / Kursiv</summary>
+        public static string EndItalic() => "</i>";
+
+        /// <summary>underlined / Unterstrichen</summary>
+        public static string StartUnderline() => "<u>";
+        /// <summary>underlined / Unterstrichen</summary>
+        public static string EndUnderline() => "</u>";
+
+        /// <summary>Crossed Out / Durchgestrichen</summary>
+        public static string StartCrossOut() => "<s>";
+        /// <summary>Crossed Out / Durchgestrichen</summary>
+        public static string EndCrossOut() => "</s>";
+
+        /// <summary>Superscript / Hochgestellt</summary>
+        public static string StartSuperscript() => "<sup>";
+        /// <summary>Superscript / Hochgestellt</summary>
+        public static string EndSuperscript() => "</sup>";
+
+        /// <summary>Subscript / Tiefgestellt</summary>
+        public static string StartSubscript() => "<sub>";
+        /// <summary>Subscript / Tiefgestellt</summary>
+        public static string EndSubscript() => "</sub>";
+
+    }
+}
