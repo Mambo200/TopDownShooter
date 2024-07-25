326555a44f205b3e5ce6cb09eddfed9f60bdbd51
Tobias Stroedicke <tobis-stroedicke@gmx.de>
2024-07-25 23:00:53 +0200

    Add new enemy (Spider)

--- Gerry gets out/Assets/Scripts/Enemy/Spider.cs
+++ Gerry gets out/Assets/Scripts/Enemy/Spider.cs
@@ -2,17 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Spider : MonoBehaviour
+public class Spider : BaseEnemy
 {
     // Start is called before the first frame update
-    void Start()
+    override protected void Start()
     {
-        
+        base.Start();
     }
 
     // Update is called once per frame
-    void Update()
+    override protected void Update()
     {
-        
+        base.Update();
     }
 }
